905a5440fff6794ea5cfaf4bc27d7c3485252ca2
Retae18 <35357413+Retae18@users.noreply.github.com>
2018-01-31 14:52:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factor_GivenNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int num, x;
+
+            Console.WriteLine("Enter a number:");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("factors: ");
+
+            for (x = 1; x <= num; x++)
+            {
+                if (num % x== 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
